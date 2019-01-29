9997bc51039aa2ad38d20432b979677c33e71a5c
juansanluq <juansanluq@gmail.com>
2019-01-29 20:32:31 +0100

    Login implementado
    Hago el login del usuario, guardo el token del mismo para futuras consultas a la API

---
+++ app/src/main/java/com/example/masterrecipes/UserToken.java
@@ -0,0 +1,17 @@
+package com.example.masterrecipes;
+
+public class UserToken {
+    String key;
+
+    public UserToken(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
