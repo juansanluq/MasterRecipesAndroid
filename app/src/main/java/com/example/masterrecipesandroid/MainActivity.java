03a183a149a1e7335eb738f119b0f814e11d9faa
juansanluq <juansanluq@gmail.com>
2019-02-02 17:46:49 +0100

    Añadido el Datepicker y el ImagePicker

--- app/src/main/java/com/example/masterrecipesandroid/MainActivity.java
+++ app/src/main/java/com/example/masterrecipesandroid/MainActivity.java
@@ -34,5 +34,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Registro.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/masterrecipesandroid/Registro.java
+++ app/src/main/java/com/example/masterrecipesandroid/Registro.java
@@ -1,11 +1,25 @@
 package com.example.masterrecipesandroid;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,19 +29,20 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import java.io.IOException;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.Manifest.permission_group.CAMERA;
 
 public class Registro extends AppCompatActivity {
 
@@ -45,6 +60,8 @@ public class Registro extends AppCompatActivity {
     EditText edtApellidos;
     @BindView(R.id.edtFecha)
     EditText edtFecha;
+    @BindView(R.id.tilFecha)
+    TextInputLayout tilFecha;
     @BindView(R.id.edtEmail)
     EditText edtEmail;
     @BindView(R.id.edtTelefono)
@@ -56,52 +73,169 @@ public class Registro extends AppCompatActivity {
     @BindView(R.id.btnRegister)
     Button btnRegister;
 
+    int CAMERA_REQUEST = 1;
+    int RESULT_LOAD_IMG = 2;
+    Bitmap photo;
+    Context contexto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
         ButterKnife.bind(this);
+        contexto = getApplicationContext();
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-                String url = "http://192.168.1.6:80/api/1.0/usuarios/";
-                StringRequest sr = new StringRequest(Request.Method.POST, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error)
-                            {
-                                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
-                                Log.e("Error: ",error.toString());
-                            }
-                        }) {
-                    @Override
-                    protected Map<String, String> getParams() {
-                        Map<String, String> params = new HashMap<String, String>();
-                        params.put("username", edtUsername.getText().toString());
-                        params.put("password", edtPassword.getText().toString());
-                        params.put("nombre", edtNombre.getText().toString());
-                        params.put("apellidos", edtApellidos.getText().toString());
-                        params.put("fecha_nacimiento", edtFecha.getText().toString());
-                        params.put("email", edtEmail.getText().toString());
-                        params.put("numero_telefono", edtTelefono.getText().toString());
-                        params.put("comentarios", edtComentario.getText().toString());
-                        return params;
-                    }
+                RegistrarUsuario();
+            }
+        });
 
-                };
-                requestQueue.add(sr);
+        edtFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                obtenerFecha();
+            }
+        });
+
+        imgPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Registro.this);
+                builder.setTitle("Selecciona método de entrada")
+                        .setItems(R.array.opcionesEntrada, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                if(which == 0)
+                                {
+                                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                                }
+                                if(which == 1)
+                                {
+                                    if (checkPermission()) {
+                                        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                                        intent.setType("image/*");
+                                        startActivityForResult(intent, RESULT_LOAD_IMG);
+                                    }
+                                }
+                            }
+                        });
+                builder.create().show();
             }
         });
 
 
+    }
 
+    private void RegistrarUsuario() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        String url = "http://192.168.1.6:80/api/1.0/usuarios/";
+        StringRequest sr = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
+                        Log.e("Error: ", error.toString());
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("username", edtUsername.getText().toString());
+                params.put("password", edtPassword.getText().toString());
+                params.put("nombre", edtNombre.getText().toString());
+                params.put("apellidos", edtApellidos.getText().toString());
+                params.put("fecha_nacimiento", edtFecha.getText().toString());
+                params.put("email", edtEmail.getText().toString());
+                params.put("numero_telefono", edtTelefono.getText().toString());
+                params.put("comentarios", edtComentario.getText().toString());
+                return params;
+            }
+
+        };
+        requestQueue.add(sr);
+    }
+
+    private void obtenerFecha() {
+        final String CERO = "0";
+        final String BARRA = "/";
+
+        //Calendario para obtener fecha & hora
+        final Calendar c = Calendar.getInstance();
+
+        //Variables para obtener la fecha
+        final int mes = c.get(Calendar.MONTH);
+        final int dia = c.get(Calendar.DAY_OF_MONTH);
+        final int anio = c.get(Calendar.YEAR);
+
+        DatePickerDialog recogerFecha = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                //Esta variable lo que realiza es aumentar en uno el mes ya que comienza desde 0 = enero
+                final int mesActual = month + 1;
+                //Formateo el día obtenido: antepone el 0 si son menores de 10
+                String diaFormateado = (dayOfMonth < 10) ? CERO + String.valueOf(dayOfMonth) : String.valueOf(dayOfMonth);
+                //Formateo el mes obtenido: antepone el 0 si son menores de 10
+                String mesFormateado = (mesActual < 10) ? CERO + String.valueOf(mesActual) : String.valueOf(mesActual);
+                //Muestro la fecha con el formato deseado
+                edtFecha.setText(year + BARRA + mesFormateado + BARRA + diaFormateado);
+
+
+            }
+            //Estos valores deben ir en ese orden, de lo contrario no mostrara la fecha actual
+            /**
+             *También puede cargar los valores que usted desee
+             */
+        }, anio, mes, dia);
+        //Muestro el widget
+        recogerFecha.show();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            photo = (Bitmap) data.getExtras().get("data");
+            imgPerfil.setImageBitmap(photo);
+        }
+        if(requestCode == RESULT_LOAD_IMG && resultCode == Activity.RESULT_OK)
+        {
+            Uri uri = data.getData();
+            try {
+                photo = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                imgPerfil.setImageBitmap(photo);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean checkPermission() {
+        /* Se compureba de que la SDK es superior a marshmallow, pues si es inferior no es necesario
+         * pedir permisos */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if ((checkSelfPermission(CAMERA) != PackageManager.PERMISSION_GRANTED) &&
+                    (checkSelfPermission(WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) &&
+                    (checkSelfPermission(READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
+                /* En caso de no haber cargado correctamente los permisos se avisa con
+                 * un Toast y se piden */
+                Toast.makeText(getApplicationContext(), "Error al cargar permisos", Toast.LENGTH_LONG).show();
+                requestPermissions(new String[]{CAMERA, WRITE_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE}, 100);
+                return false;
+            } else {
+                /* En caso de todos los permisos correctos se notifica */
+                Toast.makeText(getApplicationContext(), "Todos los permisos se han cargado correctamente", Toast.LENGTH_LONG).show();
+                return true;
+            }
+        }
+        return true;
     }
 }
